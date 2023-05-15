9700408ce6615b3778c9be410adec368f7f29b31
Ivan Kochelorov <ivanko4elorov@gmail.com>
2023-05-15 14:44:53 +0700

    Added click controlling

--- scripts.js
+++ scripts.js
@@ -14,10 +14,10 @@ function shuffle(array) {
     return array;
 }
 
-const hoveredWithDragOpacity = "0.7";
+const hoveredWithDragOpacity = "0.5";
 const termsCount = 3;
 
-var draggedCharacteristic;
+var selectedCharacteristic;
 var hoveredWithDragCharacteristic;
 var characteristicBaseColor = "rgb(255, 255, 255)";
 
@@ -36,7 +36,7 @@ function refreshTask() {
     while (termIndex < termsCount) {
         let random = getRandomInt(data.length);
         if (!termsIndex.includes(random)) {
-            termsHTML[termIndex] = '<div class="definition"><div class="dropTarget" id="' + data[random].id + '">' + data[random].term + '</div></div>';
+            termsHTML[termIndex] = '<div class="definition"><div class="term" id="' + data[random].id + '">' + data[random].term + '</div></div>';
             termsIndex.push(random);
             termIndex++;
         }
@@ -71,80 +71,99 @@ function refreshTask() {
         characteristicsContainer.innerHTML = characteristicsContainer.innerHTML + characteristicHTML;
     });
 }
+function selectCharacteristic(characteristic) {
+    selectedCharacteristic = characteristic;
+    selectedCharacteristic.style.opacity = hoveredWithDragOpacity;
+    selectedCharacteristic.style.backgroundColor = characteristicBaseColor;
+}
+function unselectCharacteristic() {
+    if (selectedCharacteristic != null) {
+        selectedCharacteristic.style.opacity = "1";
+        selectedCharacteristic = null;
+    }
+}
+function hoverWithSelectedCharacteristic(hoverTarget) {
+    hoveredWithDragCharacteristic = hoverTarget;
+    hoveredWithDragCharacteristic.style.opacity = hoveredWithDragOpacity;
+}
+function unhoverWithSelectedCharacteristic() {
+    if (hoveredWithDragCharacteristic != null) {
+        hoveredWithDragCharacteristic.style.opacity = "1";
+        hoveredWithDragCharacteristic = null;
+    }
+}
+function attachCharacteristic(definition, characteristic) {
+    definition.appendChild(characteristic);
+    unselectCharacteristic();
+}
+function deattachCharacteristic(characteristic) {
+    document.getElementById("characteristicsContainer").appendChild(characteristic);
+    unselectCharacteristic();
+}
 
 window.onload = function () {
     refreshTask();
 
-    document.getElementById("refresh").addEventListener("click", refreshTask);
-
-    document.addEventListener("dragstart", function (event) {
-        draggedCharacteristic = event.target;
-        draggedCharacteristic.style.opacity = hoveredWithDragOpacity;
-        draggedCharacteristic.style.backgroundColor = characteristicBaseColor;
+    Array.from(document.getElementsByClassName("characteristic")).forEach(characteristic => {
+        characteristic.addEventListener("click", function (event) {
+            unselectCharacteristic();
+            selectCharacteristic(event.target);
+        });
+    });
+    Array.from(document.getElementsByClassName("term")).forEach(term => {
+        term.addEventListener("click", function (event) {
+            attachCharacteristic(event.target.parentElement, selectedCharacteristic);
+        });
     });
 
+    document.getElementById("refresh").addEventListener("click", refreshTask);
+    document.addEventListener("dragstart", function (event) {
+        unselectCharacteristic();
+        selectCharacteristic(event.target);
+    });
     document.addEventListener("drag", function (event) {
 
     });
-
     document.addEventListener("dragend", function (event) {
         event.target.style.opacity = "1";
     });
-
     document.addEventListener("dragenter", function (event) {
-        if (event.target.className == "dropTarget") {
-            event.target.style.opacity = hoveredWithDragOpacity
-        }
-        if (event.target.className == "characteristic") {
-            hoveredWithDragCharacteristic = event.target;
-            hoveredWithDragCharacteristic.style.opacity = hoveredWithDragOpacity;
+        if (event.target.className == "term" || event.target.className == "characteristic") {
+            hoverWithSelectedCharacteristic(event.target);
         }
     });
-
     document.addEventListener("dragover", function (event) {
         event.preventDefault();
     });
-
     document.addEventListener("dragleave", function (event) {
-        if (event.target.className == "dropTarget") {
-            event.target.style.opacity = "1";
-        }
-        if (event.target.className == "characteristic") {
-            hoveredWithDragCharacteristic.style.opacity = "1";
+        if (event.target.className == "term" || event.target.className == "characteristic") {
+            unhoverWithSelectedCharacteristic();
         }
     });
-
     document.addEventListener("drop", function (event) {
         event.preventDefault();
+        unhoverWithSelectedCharacteristic();
         let targetDiv = event.target;
-        if (targetDiv.className == "dropTarget") {
-            targetDiv.style.opacity = 1;
-            targetDiv.parentElement.appendChild(draggedCharacteristic);
-            draggedCharacteristic = null;
+        if (targetDiv.className == "term") {
+            attachCharacteristic(targetDiv.parentElement, selectedCharacteristic)
         }
         else if (targetDiv.className == "definition") {
-            targetDiv.appendChild(draggedCharacteristic);
-            draggedCharacteristic = null;
+            attachCharacteristic(targetDiv, selectedCharacteristic)
         }
         else if (targetDiv.className == "characteristic") {
-            targetDiv.style.opacity = 1;
             let targetDivParent = targetDiv.parentElement;
-
             if (targetDivParent.className == "definition") {
-                draggedCharacteristic.parentElement.appendChild(targetDiv);
-
-                targetDivParent.appendChild(draggedCharacteristic);
-                draggedCharacteristic = null;
+                attachCharacteristic(targetDivParent, selectedCharacteristic);
+                deattachCharacteristic(targetDiv);
             }
             else {
-                document.getElementById("characteristicsContainer").appendChild(draggedCharacteristic);
+                deattachCharacteristic(selectedCharacteristic);
             }
         }
         else {
-            document.getElementById("characteristicsContainer").appendChild(draggedCharacteristic);
+            deattachCharacteristic(selectedCharacteristic);
         }
     });
-
     document.getElementById("check").addEventListener("click", function () {
         let definitionsHTMLCollection = document.getElementsByClassName("definition");
         let definitions = Array.from(definitionsHTMLCollection);
@@ -153,7 +172,7 @@ window.onload = function () {
             let characteristics = Array.from(characteristicsHTMLCollection);
             characteristics.forEach(characteristic => {
                 if (characteristic != null) {
-                    if (characteristic.id == definition.getElementsByClassName("dropTarget")[0].id) {
+                    if (characteristic.id == definition.getElementsByClassName("term")[0].id) {
                         characteristic.style.backgroundColor = "rgb(200, 255, 200)";
                     }
                     else {
